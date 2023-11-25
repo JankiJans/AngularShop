77b52e05cbc5d5bc51e6ce4dabd80b268ab65615
Jan Kędzierski <jkedzierski087@gmail.com>
2023-11-25 12:03:25 +0100

    adding some content to recipes components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipes.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2023/11/18/16/09/pears-8396722_960_720.jpg')
+  ]
   constructor() {
 
   }
